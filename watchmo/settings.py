2c8a22044dda50875f5122108b37edc8ca4f50b2
Muchhar Bharat <42728919+muchhar@users.noreply.github.com>
2024-06-01 15:10:14 +0530

    Update settings.py

--- watchmo/settings.py
+++ watchmo/settings.py
@@ -227,6 +227,6 @@ CORS_ALLOWED_ORIGINS = [
 # Define the media root for user-uploaded files
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'    
 
